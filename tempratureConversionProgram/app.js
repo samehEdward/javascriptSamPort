fa53459838d6bd068a15692f0bfb371148e31006
Sameh Edward <edw21629@spengergasse.at>
2023-09-23 13:03:09 +0200

    Code format

--- tempratureConversionProgram/app.js
+++ tempratureConversionProgram/app.js
@@ -1,33 +1,25 @@
+document.getElementById("calcButton").onclick = function () {
+  if (document.getElementById("cel").checked) {
+    let temp;
+    temp = document.getElementById("inputTemp").value;
+    temp = Number(temp);
+    temp = clacCelsius(temp);
+    document.getElementById("result").innerHTML = temp + "°C";
+  } else if (document.getElementById("fah").checked) {
+    let temp;
+    temp = document.getElementById("inputTemp").value;
 
-document.getElementById("calcButton").onclick = function(){
+    temp = Number(temp);
+    temp = calcFahrenheit(temp);
+    document.getElementById("result").innerHTML = temp + "°F";
+  } else
+    [(document.getElementById("result").innerHTML = "Select Temprature type")];
+};
 
-    if(document.getElementById("cel").checked){
-
-        let temp;
-        temp = document.getElementById("inputTemp").value;
-
-        temp = Number(temp);
-        temp = clacCelsius(temp);
-        document.getElementById("result").innerHTML = temp + "°C"
-
-    }
-    else if(document.getElementById("fah").checked){
-        let temp;
-        temp = document.getElementById("inputTemp").value;
-
-        temp = Number(temp);
-        temp = calcFahrenheit(temp);
-        document.getElementById("result").innerHTML = temp + "°F"
-    }
-    else[
-        document.getElementById("result").innerHTML = "Select Temprature type"
-    ]
+function clacCelsius(temp) {
+  return (temp - 32) * (5 / 9);
 }
 
-function clacCelsius(temp){
-    return (temp - 32) * (5/9);
-}
-
-function calcFahrenheit(temp){
-    return temp * 9 / 5 + 32;
+function calcFahrenheit(temp) {
+  return (temp * 9) / 5 + 32;
 }
