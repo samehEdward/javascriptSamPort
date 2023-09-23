f63340136c807f24839af21498c2c4827cb42720
Sameh Edward <edw21629@spengergasse.at>
2023-09-23 19:11:05 +0200

    2d Array

--- array/app.js
+++ array/app.js
@@ -1,6 +1,44 @@
-let prices = [5, 7, 3, 111, 777, 137];
+// let prices = [5, 7, 3, 111, 777, 137];
 
-for(let i = 0; i<prices.length; i+=1)
+// for(let i = 0; i<prices.length; i+=1)
+// {
+//     console.log(prices[i]);
+// }
+
+// for(let price of prices){
+//     console.log(price);
+// }
+
+/////////////////////////////////////////////////////////////////////////////
+
+//let fruits = ["banana", "apple", "orange", "mango"];
+
+//fruits = fruits.sort();
+
+// fruits = fruits.sort().reverse();
+
+
+// for(let fruit of fruits)
+// {
+//     console.log(fruit);
+// }
+
+////////////////////////////////////////////////////////////////////////////////
+
+// 2D Array
+
+let fruits =     ["apples", "oranges", "bananas"];
+let vegetables = ["carrots", "onions", "potatoes"];
+let meats =      ["eggs", "chiken", "fish"];
+
+let groceryList = [fruits, vegetables, meats];
+
+   console.log(groceryList[2][0]);
+
+for(let list of groceryList)
 {
-    console.log(prices[i]);
+    for(let one of list)
+    {
+        console.log(one);
+    }
 }
